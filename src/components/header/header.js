d2e3a5f585de0354306e1ce78ebc368c01af2c8a
Victory Onuoha <Victoryonuoha@cmail.carleton.ca>
2022-11-06 15:01:56 -0500

    modified header to add logo

---
+++ src/components/header/header.js
@@ -0,0 +1,13 @@
+import logo from '../../assets/logo.png'
+
+function Header() {
+    return (
+        <header className='header' >
+            <section className='header__logo-box' >
+                <img src={logo} className='header__logo' alt='website logo' />
+            </section>
+        </header>
+    )
+}
+
+export default Header
