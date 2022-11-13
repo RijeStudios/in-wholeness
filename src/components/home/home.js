02bdb8591660cbac5cf2bd8082a78353f36807bb
Victory Onuoha <Victoryonuoha@cmail.carleton.ca>
2022-11-12 19:06:17 -0500

    created markup for why section

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,4 +1,5 @@
 import smile from '../../assets/images/smile.jpg';
+import why from '../../assets/images/why.jpg' ;
 import './home.scss'
 function Home() {
     return (
@@ -8,25 +9,40 @@ function Home() {
                     <img src={smile} className='home__hero-image' alt='healthy woman smiling because her condition has transformed' />
                 </figure>
 
-            <div className='home__main-box' > 
-                <article className='home__text-box' >
-                    <h1 className='home__hero-title' >Take charge of your gut health!</h1>
-                    <p className='home__hero-text' >Do you feel like no matter what diet plan you
-                        try, your condition doesn't improve?
-                    </p>
-                    <p className='home__hero-text' >Talk to our specialists to transform your life
-                        from illness to wholeness.
-                    </p>
-                </article>
-                <article className='home__button-box' >
-                    <button className='home__button' > BOOK APPOINTMENT</button>
-                </article>
-            </div>
+                <div className='home__main-box' > 
+                    <article className='home__text-box' >
+                        <h1 className='home__hero-title' >Take charge of your gut health!</h1>
+                        <p className='home__hero-text' >Do you feel like no matter what diet plan you
+                            try, your condition doesn't improve?
+                        </p>
+                        <p className='home__hero-text' >Talk to our specialists to transform your life
+                            from illness to wholeness.
+                        </p>
+                    </article>
+                    <article className='home__button-box' >
+                        <button className='home__button' > BOOK APPOINTMENT</button>
+                    </article>
+                </div>
                
             </section>
-            <div className='home__line' >
-
-            </div>
+            <div className='home__line' ></div>
+            <section className='home__about' >
+                <h2>WHY <span>IN WHOLENESS ?</span> </h2>
+                <article>
+                    <ul>
+                        <li>Access to Coaching and Courses</li>
+                        <li>Scientific & Holistic approach</li>
+                        <li>Get support via pur Fitness, Nutrition & Psychotherapy programs</li>
+                        <li>Personalized Nutrition & more</li>
+                    </ul>
+                </article>
+                <article>
+                    <button> WHY IT WORKS</button>
+                </article>
+                <figure>
+                    <img src={why} className='' />
+                </figure>
+            </section>
             
         </main>
     )
