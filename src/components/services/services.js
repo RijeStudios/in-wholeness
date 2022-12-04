8351d31a0011058784ea6680c341db035694b55e
Victory Onuoha <Victoryonuoha@cmail.carleton.ca>
2022-12-03 23:41:23 -0500

    created styling for services page for mobile queries

--- src/components/services/services.js
+++ src/components/services/services.js
@@ -2,19 +2,21 @@ import why from '../../assets/images/why.jpg';
 import nutritionConsult from '../../assets/images/nutritionConsult.jpg';
 import training from '../../assets/images/training.jpg';
 import lifeCoaching from '../../assets/images/lifeCoaching.jpg';
+import './services.scss';
+
 
 function Services() {
     return (
 
         <main className="services" >
-           <article>
-                <section className="services">
-                    <h1>From Illness to Wellness.</h1>
-                    <h1>We are committed.</h1>
-                    <p>98% of our clients see a transformation in the first 6 months *</p>
+           <article className='services__header-box' >
+                <section className="services__header">
+                    <h1 className='services__title' >From Illness to Wellness.</h1>
+                    <h1 className='services__title' >We are committed.</h1>
+                    <p className='services__study' >98% of our clients see a transformation in the first 6 months *</p>
                 </section>
-                <figure>
-                    <img src={why} alt='services by in wholeness'  />
+                <figure className='services__image-box' >
+                    <img src={why} className='services__header-image' alt='services by in wholeness'  />
                 </figure>
            </article>
 
@@ -22,60 +24,60 @@ function Services() {
                 <div className='home__line'></div>
             </div>
 
-            <section>
-                <h2>SERVICES <span> WE PROVIDE</span></h2>
+            <section className='services__title-box' >
+                <h2 className='services__title-header' >SERVICES <span className='services__title-header--gold' > WE PROVIDE</span></h2>
             </section>
 
-            <article>
+            <article className='services__service' >
                 <section>
-                    <figure className='' >
-                        <img src={nutritionConsult} className='' alt='man searching for nutritionist' />
+                    <figure className='services__image-box' >
+                        <img src={nutritionConsult} className='services__image-consult' alt='man searching for nutritionist' />
                     </figure> 
-                    <h3 className='' >Nutrition Consult:</h3>
-                    <h4 className='' >
+                    <h3 className='services__type' >Nutrition Consult:</h3>
+                    <h4 className='services__description' >
                         Get a personalized diet plan using 
                         genetic testing to understand your digestive health.
                     </h4>
-                    <h5> Target areas: Food allergies, IBD, Fertility, Pregnancy,
+                    <h5 className='services__tag' > Target areas: Food allergies, IBD, Fertility, Pregnancy,
                          Post partum Weight loss, Diabetes, Hormonal health, Meal planning,
                          Genetic testing
                     </h5>
-                    <section>
-                        <a>BOOK APPOINTMENT</a>
+                    <section className='services__button-box' >
+                        <a className='services__button' >BOOK APPOINTMENT</a>
                     </section>
                 </section>
 
                 <section>
-                    <figure className='' >
-                        <img src={training} className='' alt='person doing personalised workout' />
+                    <figure className='services__image-box' >
+                        <img src={training} className='services__image' alt='person doing personalised workout' />
                     </figure> 
-                    <h3 className='' >Personal Training:</h3>
-                    <h4 className='' >
+                    <h3 className='services__type' >Personal Training:</h3>
+                    <h4 className='services__description' >
                         Get access to our digital vault of targeted exercise plans
                         to help you fall in love with movement again. 
                     </h4>
-                    <h5> Target areas: Weight loss & management, Personalized nutrition,
+                    <h5 className='services__tag' > Target areas: Weight loss & management, Personalized nutrition,
                          Fitness plans
                     </h5>
-                    <section>
-                        <a>BOOK APPOINTMENT</a>
+                    <section className='services__button-box' >
+                        <a className='services__button' >BOOK APPOINTMENT</a>
                     </section>
                 </section>
 
                 <section>
-                    <figure className='' >
-                        <img src={lifeCoaching} className='' alt='person doing a life coaching session' />
+                    <figure className='services__image-box' >
+                        <img src={lifeCoaching} className='services__image' alt='person doing a life coaching session' />
                     </figure> 
-                    <h3 className='' >Life Coaching:</h3>
-                    <h4 className='' >
+                    <h3 className='services__type' >Life Coaching:</h3>
+                    <h4 className='services__description' >
                         Connect the dots between your body and mind and learn how
                         to balance nutrition and mental wellness.
                     </h4>
-                    <h5> Target areas: Anxiety, Depression, Post partum Depression,
+                    <h5 className='services__tag' > Target areas: Anxiety, Depression, Post partum Depression,
                          Stress, Burnout, Mental support
                     </h5>
-                    <section>
-                        <a>BOOK APPOINTMENT</a>
+                    <section className='services__button-box' >
+                        <a className='services__button' >BOOK APPOINTMENT</a>
                     </section>
                 </section>
 
