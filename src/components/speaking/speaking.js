b8117b1e4943ffaf0eea4867ce0956d20ec66887
Victory Onuoha <Victoryonuoha@cmail.carleton.ca>
2022-12-07 18:50:16 -0500

    created html for speaking page

---
+++ src/components/speaking/speaking.js
@@ -0,0 +1,74 @@
+
+
+function Speaking() {
+  return (
+    <main className="speaking">
+      <section>
+        <h1>BRING SOME WHOLENESS TO YOUR DAY</h1>
+        <h3>Engaging and inspiring health & wellness speaker</h3>
+      </section>
+
+      <section>
+        <figure>
+            <img src='' alt='christie noua a health and wellness speaker' />
+        </figure>
+        <article>
+            <p>Christie Noua has inspired and educated thousands of
+                audiences across North america. With a diverse portfolio of
+                wellness and fitness workshops. 
+            </p>
+
+            <p> Our most requested topic is “Weight loss management”. You can also Choose from our
+                extensive list of topics or work with us to create a
+                 customised soultuion that suits your events
+            </p>
+
+            <section>
+                <a> CONTACT US</a>
+            </section>
+        </article>
+
+      </section>
+    
+      <section>
+        <h2>SPEAKING <span>TOPICS</span> </h2>
+        <ul>
+            <li>Lean, Mean, Green: How to loose and maintain
+                weight with a plant based lifestyle
+            </li>
+            <li>Gut on Lock: Transform your gut health using our 3 pillars 
+                of gut health
+            </li>
+            <li>New Me, New Brain: How to manage your nutrition
+                and mental health during times of high stress
+            </li>
+        </ul>
+      </section>
+       
+      <section>
+        <h2> ABOUT <span> CHRISTIE NOUA</span></h2>
+      </section>
+
+      <section>
+        <article>
+            <h5>
+            Christie Noua is a board certified nutritionist, personal 
+            coach and registered psychotherapist (qualifying).
+            </h5>
+            <h6> Clinical expertise: Digestive health, Endocrine disorders
+                Hormonal and Mental issues
+            </h6>
+        </article>
+        <figure>
+            <img src='' alt='christie noua a certified nutritionist and wellness speaker' />
+        </figure>
+      </section>
+      <section>
+        <a>Download Resume</a>
+        <a> WORK WITH ME</a>
+      </section>
+    </main>
+  );
+}
+
+export default Speaking;
