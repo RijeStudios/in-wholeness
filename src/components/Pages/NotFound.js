d67da08fbad966762478387031971cd20ba469ac
Victory Onuoha <Victoryonuoha@cmail.carleton.ca>
2022-12-29 00:38:20 -0500

    added styles to the 404 page across all media queries

--- src/components/Pages/NotFound.js
+++ src/components/Pages/NotFound.js
@@ -1,9 +1,24 @@
+import { Link } from "react-router-dom";
+import './NotFound.scss';
+import { FiArrowRight} from 'react-icons/fi';
+import NotFoundImage from '../../assets/images/404-image.svg'
 
 function NotFound() {
   return (
-    <section>
-      <h1>404</h1>
-      <p>Page not found</p>
+    <section className='notfound' >
+      <h1 className='notfound__title' >Oh no, what are you doing here?</h1>
+      <p className='notfound__text' >
+        Good nutrition exists! 
+        You can find it at our home base.
+      </p>
+      <article className='notfound__image-box' >
+        <img src={NotFoundImage} alt='not found' className='notfound__image' />
+      </article>
+        <article>
+          <Link to='/' className='notfound__link' > 
+            Go back home <FiArrowRight/>
+          </Link>
+        </article>
     </section>
   );
 }
