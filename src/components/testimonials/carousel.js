669f2965421811fe9ec0d5cdec31125bb53d7c04
Victory Onuoha <Victoryonuoha@cmail.carleton.ca>
2023-03-02 20:19:36 -0500

    updated styling for testimony section

--- src/components/testimonials/carousel.js
+++ src/components/testimonials/carousel.js
@@ -10,7 +10,7 @@ export default function Carousel() {
             <Swiper className="testimonials__testimony-container mySwiper" navigation={true} modules={[Navigation]}>
                 {Testimonial.map((item, index) => {
                     return (
-                        <SwiperSlide key={index}>
+                        <SwiperSlide key={index} className='testimonials__swiper-box' >
                             <h2 className='testimonials__title-header' >{item.name}</h2>
                             <p className='testimonials__testimony' >{item.content}</p>
                         </SwiperSlide>
